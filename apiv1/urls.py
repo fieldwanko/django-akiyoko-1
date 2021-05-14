a7aee26adede8be204c35f89da633fd5d4cd679e
world field <worldfield@warudonoMacBook-Pro.local>
2021-05-15 00:12:37 +0900

    Create config/urls and apiv1/urls

---
+++ apiv1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers, urlpatterns
+from . import views
+
+router = routers.DefaultRouter()
+router.register('books', views.BookViewSet)
+
+app_name = 'apiv1'
+urlpatterns = [
+    path('', include(router.urls)),
+]
