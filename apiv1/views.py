e783977d4804fa8da929d87cc938ed77b338418c
world field <worldfield@warudonoMacBook-Pro.local>
2021-05-15 00:01:45 +0900

    Create serializer and ModelViewSet

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from shop.models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    """本モデルのCRUD用APIクラス"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
